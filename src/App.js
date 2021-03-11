05432a4ff4936ce2d8a681a2749d76ec8ee3cbdd
pierrolabo <pierrolabo@gmail.com>
2021-03-11 17:43:16 +0100

    done for mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header/Header';
+import Filter from './components/Filter/Filter';
+
 import './styles/main.scss'
 
 function App() {  
   return (
     <div className="App">
       <Header></Header>
+      <Filter/>
     </div>
   );
 }
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,25 @@
+import filterLogo from '../../assets/mobile/icon-filter.svg'
+import searchLogo from '../../assets/desktop/icon-search.svg'
+
+import './Filter.scss'
+const Filter = () => {
+    return (
+        <div className="filter">
+            <div className="filter__container">
+            <input type="text" className="filter__container--input" placeholder="Filter by title..."/>
+            <div className="filter__container__icons">
+
+            <div className="filter__container__icons--filterlogo__container">
+                <div className="filter__container__icons--filterlogo__container--logo"/>
+                
+            </div>
+            <div className="filter__container__icons--searchlogo__container">
+                <img src={searchLogo} alt=""/>
+            </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Filter;
