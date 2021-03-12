d1da1730263e3a04e3f400c3ab7472ccecf4fa73
pierrolabo <pierrolabo@gmail.com>
2021-03-12 17:56:21 +0100

    ok for desktop and tablet

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,5 +1,3 @@
-import searchLogo from '../../assets/desktop/icon-search.svg'
-
 import './Filter.scss'
 const Filter = () => {
     return (
@@ -11,9 +9,20 @@ const Filter = () => {
                     </div>
                     <input type="text" className="filter__searchField--input" placeholder="Filter by title..."/>
                 </div>
+                <div className="filter__searchOptions__filterByLocation hide-on-mobile">
+                    <div className="filter__searchOptions__filterByLocation__logo ">
+                        <div className="filter__searchOptions__filterByLocation__logo--locationLogo"></div>
+                    </div>
+                    <input type="text" className="filter__searchOptions__filterByLocation--input " placeholder="Filter by location..."/>
+
+                </div>
             <div className="filter__searchOptions">
-                <div className="filter__searchOptions__filter">
+                <div className="filter__searchOptions__filter hide-on-tablet">
                     <div className="filter__searchOptions__filter--logo hide-on-tablet"/>
+                </div>
+                <div className="filter__searchOptions__timeOption hide-on-mobile">
+                    <input type="checkbox" name="timeoption" id="timeoption"className="filter__searchOptions__timeOption--input"/>
+                    <label for="timeoption">Full Time</label>
                 </div>
                 <div className="filter__searchOptions__searchButton">
                     <div className="filter__searchOptions__searchButton--logo"><span className="filter__searchOptions__searchButton--text hide-on-mobile">Search</span></div>
