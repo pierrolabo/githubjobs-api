6057c83f098296b33c55beba7fc3c5960ef2784f
pierrolabo <pierrolabo@gmail.com>
2021-03-16 19:36:22 +0100

    reusable inputTimeOption

--- src/components/Inputs/InputTimeOption.js
+++ src/components/Inputs/InputTimeOption.js
@@ -1,8 +1,22 @@
-const InputTimeOption = () => {
+/*
+const InputTimeOption = ({type, placeholder, parentName}) => {
     return (
-        <div className="filter__searchOptions__timeOption hide-on-mobile">
+        <div className={`filter__searchOptions__timeOption ${type === "MOBILE" ? "hide-on-mobile" : ""}`}>
             <input type="checkbox" name="timeoption" id="timeoption"className="filter__searchOptions__timeOption--input"/>
-            <label for="timeoption">Full Time</label>
+            <label for="timeoption">{placeholder}</label>
+        </div>
+    )
+}
+
+export default InputTimeOption;
+
+*/
+
+const InputTimeOption = ({type, placeholder, parentName}) => {
+    return (
+        <div className={`${parentName}__inputTimeOption ${type === "MOBILE" ? "hide-on-mobile" : ""}`}>
+            <input type="checkbox" name="inputTimeOption" id="inputTimeOption"className={`${parentName}__inputTimeOption--input`}/>
+            <label for="inputTimeOption">{placeholder}</label>
         </div>
     )
 }
