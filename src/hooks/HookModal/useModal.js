a5394542336c0dcd8f6f56c7a857d3b633d1b669
pierrolabo <pierrolabo@gmail.com>
2021-03-16 22:46:14 +0100

    isShowing: false by default

--- src/hooks/HookModal/useModal.js
+++ src/hooks/HookModal/useModal.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 
 const useModal = () => {
-    const [isShowing, setIsShowing] = useState(true);
+    const [isShowing, setIsShowing] = useState(false);
     const toggleModal = () => {
         setIsShowing(!isShowing)
         console.log("modal set to: ", !isShowing)
