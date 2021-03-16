cd835acde5c8d0f782180227815d4305d074c7c9
pierrolabo <pierrolabo@gmail.com>
2021-03-16 19:36:44 +0100

    Add modal

---
+++ src/components/ModalFilter/ModalFilter.js
@@ -0,0 +1,41 @@
+import {useRef, useContext, useCallback, useEffect} from 'react';
+import {createPortal} from 'react-dom';
+import {HookFilterContext, HookFilterProvider} from '../../hooks/HookFilter/HookFilter';
+import InputLocation from '../Inputs/InputLocation';
+import InputTimeOption from '../Inputs/InputTimeOption';
+import './ModalFilter.scss'
+
+const modalElement = document.getElementById('modal-root');
+
+const ModalFilter = ({isShowing, hide}) => {
+
+    const handleEscape = (event) => {
+        if(event.target.className === "modal__container")hide()
+    }
+    
+    useEffect(() => {
+        if(isShowing) modalElement.addEventListener('click', handleEscape, false)
+        return () => {
+            modalElement.removeEventListener('click', handleEscape, false)
+        }
+    }, [handleEscape, isShowing]);
+
+    return createPortal(
+        isShowing ? (
+            <>
+            <div className="modal__container">
+                <div className="modal">
+                    <InputLocation parentName={"modal"}/>
+                    <InputTimeOption placeholder={"Full Time Only"} parentName={"modal"}/>
+                    <div className="modal__searchButton">
+                        <p className="modal__searchButton--text">Search</p>
+                    </div>
+
+
+                </div>
+            </div>
+            </>
+        ) : null,modalElement
+    )
+}
+export default ModalFilter;
