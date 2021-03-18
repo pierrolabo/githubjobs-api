18c4a1088d88a6827b29ae8964e0c5e6ea78a1c7
pierrolabo <pierrolabo@gmail.com>
2021-03-18 19:45:57 +0100

    use reducer with context

--- src/components/Container/JobsContainer/JobsContainer.js
+++ src/components/Container/JobsContainer/JobsContainer.js
@@ -1,56 +1,24 @@
-import {useEffect, useState, useContext} from 'react';
-import {HookFilterContext} from '../../../hooks/HookFilter/HookFilter';
-import './JobsContainer.scss';
+import {useEffect, useContext} from 'react';
+import {JobsContext, DispatchContext} from '../../../hooks/JobsContext/JobsContext';
+import {ACTIONS} from '../../../constants/JobAction';
+
 import JobCard from '../../CardJob/CardJob';
 import CardJobSkeleton from '../../CardJob/CardJobSkeleton';
-import {URL_API} from '../../../constants/Constants';
+import './JobsContainer.scss';
 const JobsContainer = () => {
-    const [location, fullTime, setLocation, setFullTime, filterBy, setFilterBy, doSearch, jobs, setJobs,isLoading, setIsLoading] = useContext(HookFilterContext)
-    const [index, setIndex] = useState(0)
-    const [indexedJobs, setIndexedJobs] = useState()
+    const {dispatch} = useContext(DispatchContext)
+    const {state} = useContext(JobsContext);
+    const {jobs, indexedJobs, isLoading, isLoadingNextPage} = state;
     const getJobs = () => {
-        setIsLoading(true)
-        fetch(URL_API, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-        .then((response) => {
-            console.log("fetched: ", response)
-            return response.json()
-        })
-        .then((myJson) => {
-            setIsLoading(false)
-            console.log("myjson: ", myJson)
-            setJobs(myJson)
-        })
-    }
-    const increaseIndex = () => {
-        console.log("index: ", index, "index after: ", index +12)
-        if(index === 36) {
-            //Load page 2
-            console.log("need a page 2")
-        } else {
-            setIndex(index + 12)
+        dispatch({type: ACTIONS.DO_SEARCH, payload: {...state}})
 
-        }
+    }
+    const loadMore = () => {
+        dispatch({type: ACTIONS.LOAD_MORE, payload: {...state}})
     }
     useEffect(() => {
         getJobs()
     }, [])
-    useEffect(() => {
-            if(jobs) {
-                if(!indexedJobs) {
-                    setIndexedJobs(jobs.slice(index, index + 12))
-                    
-                } else {
-                    const newArr  = [...indexedJobs, ...jobs.slice(index, index + 12)];
-                    setIndexedJobs(newArr)
-
-                }
-            }
-    }, [index, jobs])
     return (
         <main>
             <div className="jobs__container">
@@ -61,15 +29,23 @@ const JobsContainer = () => {
                         })
                     }
                 {
-                    indexedJobs && indexedJobs.map((job, index) => {
+                    indexedJobs && indexedJobs.map((job) => {
                         return <JobCard {...job} key={job.id}/>
                     })
                 }          
+                {
+                    isLoadingNextPage && Array(12).fill(0).map((item, index) => {
+                        return <CardJobSkeleton key={index}/>
+                    })
+                }
             </div>
-                <div className="jobs__loadMoreButton" style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
-                    <button onClick={increaseIndex}>Load More</button>
-                    </div>     
-                </div>
+            {
+                !isLoadingNextPage && !isLoading && jobs.length > indexedJobs.length && (<div className="jobs__loadMoreButton" style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+                <button onClick={loadMore}>Load More</button>
+                </div> )
+            }
+            </div>     
+               
         </main>
     )
 }
