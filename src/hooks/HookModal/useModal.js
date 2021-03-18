a96ae49b7de7923da3fb84d89d27f533fc02d955
pierrolabo <pierrolabo@gmail.com>
2021-03-18 19:47:10 +0100

    remove console.log

--- src/hooks/HookModal/useModal.js
+++ src/hooks/HookModal/useModal.js
@@ -4,7 +4,6 @@ const useModal = () => {
     const [isShowing, setIsShowing] = useState(false);
     const toggleModal = () => {
         setIsShowing(!isShowing)
-        console.log("modal set to: ", !isShowing)
     }
     return {isShowing, toggleModal}
 }
